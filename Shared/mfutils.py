95f6024a60af4cc7d480e36bcb3ddc6854c21492
Noah Nuebling <noah.n.public@gmail.com>
2025-03-26 19:08:21 +0100

    Added functions to mfutils.py (While working on AI-powered translations inside update-feed branch inside mac-mouse-fix repo)
    - HumanBytes: Converts byte number into Human-Readable format.
    - exc_desc: Gets exception description for debugging
    - mfdedent: More usable alternative to textwrap.dedent
    - @deptracked: Decorator for caching computed strings and their dependencies to files – and only recomputing if a dependency changed.
    	- We also added a bunch of helper functions to implement this, like `_deptracker_stamp()`
    	- This is really useful for our AI-powered translations since having the AI re-translate stuff costs money and takes longgg.

--- Shared/mfutils.py
+++ Shared/mfutils.py
@@ -21,8 +21,273 @@ import random
 import shlex
 import platform
 import re
+import hashlib
+from pathlib import Path
+from typing import Callable
 
 from dataclasses import dataclass
+from typing import List, Union
+
+
+# 
+# Byte - Human
+#   Convert number of bytes to human-readable representation
+#   I don't think this needs to be localized.
+# 
+
+class HumanBytes: # Source: 
+    METRIC_LABELS: List[str] = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    BINARY_LABELS: List[str] = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"]
+    PRECISION_OFFSETS: List[float] = [0.5, 0.05, 0.005, 0.0005] # PREDEFINED FOR SPEED.
+    PRECISION_FORMATS: List[str] = ["{}{:.0f} {}", "{}{:.1f} {}", "{}{:.2f} {}", "{}{:.3f} {}"] # PREDEFINED FOR SPEED.
+    @staticmethod
+    def format(num: Union[int, float], metric: bool=False, precision: int=1) -> str:
+        """
+        Human-readable formatting of bytes, using binary (powers of 1024)
+        or metric (powers of 1000) representation.
+        """
+        assert isinstance(num, (int, float)), "num must be an int or float"
+        assert isinstance(metric, bool), "metric must be a bool"
+        assert isinstance(precision, int) and precision >= 0 and precision <= 3, "precision must be an int (range 0-3)"
+        unit_labels = HumanBytes.METRIC_LABELS if metric else HumanBytes.BINARY_LABELS
+        last_label = unit_labels[-1]
+        unit_step = 1000 if metric else 1024
+        unit_step_thresh = unit_step - HumanBytes.PRECISION_OFFSETS[precision]
+        is_negative = num < 0
+        if is_negative: # Faster than ternary assignment or always running abs().
+            num = abs(num)
+        for unit in unit_labels:
+            if num < unit_step_thresh:
+                # VERY IMPORTANT:
+                # Only accepts the CURRENT unit if we're BELOW the threshold where
+                # float rounding behavior would place us into the NEXT unit: F.ex.
+                # when rounding a float to 1 decimal, any number ">= 1023.95" will
+                # be rounded to "1024.0". Obviously we don't want ugly output such
+                # as "1024.0 KiB", since the proper term for that is "1.0 MiB".
+                break
+            if unit != last_label:
+                # We only shrink the number if we HAVEN'T reached the last unit.
+                # NOTE: These looped divisions accumulate floating point rounding
+                # errors, but each new division pushes the rounding errors further
+                # and further down in the decimals, so it doesn't matter at all.
+                num /= unit_step
+        return HumanBytes.PRECISION_FORMATS[precision].format("-" if is_negative else "", num, unit)
+
+#
+# Other 
+# General utility functions that don't belong together
+#
+
+def exc_desc(e: Exception) -> str:
+    return f"{type(e).__name__}({e})"
+
+def mfdedent(s: str) -> str:
+
+    # Better alternative to textwrap.dedent() 
+    # 
+    # Advantage:
+    #   Doesn't require use of `\` at  the start and end of the string to remove unwanted newlines.
+    # 
+    # Behavior:
+    #   - Dedents the string like textwrap.dedent()
+    #   - Removes exactly one newline at the beginning if present
+    #   - Removes exactly one newline at the end if present
+    #   - Preserves all other newlines and whitespace
+    # 
+    # Implementation:
+    #   We can simply check whether the firstchar/lastchar is a newline, because textwrap.dedent() removes all whitespace from empty lines – That makes things much simpler for us!
+    #
+    # Example:
+    #       
+    #       This textwrap.dedent code:
+    #           ```
+    #           textwrap.dedent("""\
+    #   
+    #               **CoolHeader**
+    #               CoolText\
+    #           """)
+    #           ```
+    #
+    #       ... is equivalent to this mfdedent code:
+    #           ```
+    #           mfdedent("""
+    #   
+    #               **CoolHeader**
+    #               CoolText
+    #           """)
+    #           ```
+
+    s = textwrap.dedent(s)
+    if s.startswith('\n'):  s = s[1:]
+    if s.endswith('\n'):    s = s[:-1]
+    return s
+
+#
+# Dependency Tracking
+#
+
+def _deptracker_get_deps(deptracker_archive_path: str) -> dict:
+    deps = None
+    try:                    deps = json.loads(Path(deptracker_archive_path).read_text())
+    except Exception as e:  deps = {}
+    assert deps is not None, f"Buggy code"
+    return deps
+
+def _deptracker_set_deps(deptracker_path: str, newdeps: dict) -> None:
+    result = json.dumps(newdeps, ensure_ascii=False, indent=4)
+    Path(deptracker_path).write_text(result)
+
+def _deptracker_hash(s: str) -> str:
+    #   Note: [Mar 2025] haven't looked into what the best hashing algorithm or library or params or anything.
+    return hashlib.md5(s.encode()).hexdigest()
+
+def _deptracker_check(deptracker_archive_path: str, source_paths: list[str], target_path: str) -> bool:
+
+    # If this returns true, the target file is still up-to-date
+    # If this returns false, the target file should be recomputed from its sources
+
+    # Declare result
+    result = None
+    reason = None
+
+    try:
+        
+        # Check if target exists
+        if not os.path.exists(target_path):
+            raise Exception(f"Target file doesn't exist at '{target_path}'")
+
+        # Load deps
+        deps = _deptracker_get_deps(deptracker_archive_path)
+
+        # Check target hashes
+        target_hash = _deptracker_hash(Path(target_path).read_text())
+        stored_target_hash = deps[target_path]['target_hash']
+        if target_hash != stored_target_hash:
+            raise Exception(f"Target file has been modified at '{target_path}'. Stored hash: {stored_target_hash}, fresh hash: {target_hash}")
+
+        # Compare source files
+        cached_source_paths = []
+        for source in deps[target_path]['sources']:
+            
+            # Store path
+            cached_source_path = source['source_path']
+            cached_source_paths.append(cached_source_path)
+            
+            # Check hash
+            cached_source_hash = source['source_hash']
+            source_hash = _deptracker_hash(Path(cached_source_path).read_text())
+            if source_hash != cached_source_hash:
+                raise Exception(f"Source file has been modified at '{cached_source_path}'")
+
+        # Check paths
+        diff = set(cached_source_paths).symmetric_difference(set(source_paths))
+        if len(diff) > 0:
+            raise Exception(f"Source files differ. diff: {diff}")
+
+        # Success
+        result = True
+
+    except Exception as e:
+        # Handle failure
+        reason = exc_desc(e)
+        result = False
+    
+    # Log
+    if result == False:
+        print(f"Dependency tracker: Check failed for file '{target_path}' with reason: {reason}")
+    elif result == True:
+        print(f"Dependency tracker: Check succeeded for file '{target_path}'.")
+    else:
+        assert False, f"Buggy code"
+
+    # Return
+    return result
+
+def _deptracker_stamp(deptracker_archive_path: str, source_paths: list[str], target_path: str) -> None:
+
+    # Call this after recomputing the target file from the source files
+
+    # Load deps
+    deps = _deptracker_get_deps(deptracker_archive_path)
+    
+    # Update deps
+    new_entry = {
+        'target_hash': _deptracker_hash(Path(target_path).read_text()),
+        'sources': [
+            { 
+                'source_path': source_path, 
+                'source_hash': _deptracker_hash(Path(source_path).read_text()) 
+            }
+            for source_path in source_paths
+        ]
+    }
+    deps[target_path] = new_entry
+
+    # Store deps
+    _deptracker_set_deps(deptracker_archive_path, deps)
+
+    # Log
+    entry_desc = f"{{'{target_path}': {new_entry}}}"
+    print(f"Dependency tracker: Recorded stamp-of-approval for target-sources relationship: {entry_desc}")
+
+def deptracked(deptracker_archive_path: str, source_paths: list[str], target_path: str):
+    def decorator(derivation_func: Callable[[None], str|None]):
+        def upget() -> str|None:
+
+            # Decorator for automatically adding dependency-tracking to a 'derivation' function
+            # Note: [Mar 2025] What does 'upget' mean?
+            #   We prefix depency-tracked functions with 'upget', e.g. 'upget_translation()' to signify that deptracked functions not only *get* and return the fresh value, but also *up*date the target_path with that fresh value.
+            #   This is important to keep in mind, because, to ensure correctness, our code needs to manually make sure that all dependency-files are updated *before* their dependent-files.
+            #   (Before we try to abstract that away, it's probably better to use an existing tool like make or snakemake or something, for now it's manageable to handle this manually)
+
+            # Check some source doesn't exist
+            #   (We only expect this during development)
+            do_skip = False
+            for p in source_paths:
+                if not os.path.exists(p):
+                    print(f"Dependency tracker: Skipping derivation of '{target_path}' because source file '{p}' doesn't exist.") # ([Mar 2025] The deptracker-archive and target-path are expected to sometimes not exist even during prod.)
+                    do_skip = True
+                    break
+            if do_skip:
+                return None
+
+            # Use cache if it's still up-to-date
+            if _deptracker_check(deptracker_archive_path, source_paths, target_path):
+                return Path(target_path).read_text()
+
+            # Do derivation
+            new_target_string = derivation_func()
+
+            # Check None
+            #   Derivation func might return None if it doesn't want to derive the string at this point.
+            if new_target_string is None:
+                return None # [Mar 2025] Should we return the (outdated) cache here?
+
+            # Validate
+            if new_target_string == (Path(target_path).read_text() if os.path.exists(target_path) else ''):
+                if 0: assert False, f"Dependency tracker: Content of '{target_path}' was already up-to-date, but our dependency tracker didn't catch that." # Not sure this validation is helpful/correct/fast enough || [Mar 2025] disabled it cause this can happen even if nothing goes wrong.
+                pass
+            else:
+                # Write updated target to file
+                p = Path(target_path)
+                p.parent.mkdir(parents=True, exist_ok=True)
+                p.write_text(new_target_string)
+
+                # Update dependency tracker
+                #   [Mar 2025] Caution:
+                #       User needs to make sure to update the source files before this is called! (Otherwise, the deptracker will reference an outdated source file)
+                #   [Mar 2025] Optimization:
+                #       - This reloads and recomputes the hash for the source_paths every time. (Inefficient since for translations there's one source file for many translations – and the source file always has the same hash.)
+                #       - This reloads the new_target_string from file – we could just pass it in.
+                #       - We could reuse file-handles from _deptracker_check() above
+                #       > Conclusion: We'll use the deptracker on slow operations, so this stuff is unlikely to matter.
+                _deptracker_stamp(deptracker_archive_path, source_paths, target_path)
+
+            # Return
+            return new_target_string
+        
+        return upget
+    return decorator
 
 #
 # Command line tools
