9f8f8ceddb727d4a21eafa4ee954399fb78abc32
Noah Nuebling <noah.n.public@gmail.com>
2025-08-17 09:20:38 +0200

    Added special rule for Aayam's request.

--- scripts/_buildmd/_buildmd.py
+++ scripts/_buildmd/_buildmd.py
@@ -774,15 +774,10 @@ def display_name(sale):
     
     # Special requests & rules
     #   Note: [Jul 2025] Update: We used to use ppls email here (in mac-mouse-fix > markdown_generator.py and in mac-mouse-fix-scripts > _buildmd.py). Don't do that anymore and look into rewriting git history if someone complains.
-
-    if name == "🇺🇦 Github Com/Alex Danilenko": # Added [Aug 2025]
-        name = "🇺🇦 Alex Danilenko"
-
-    if name == "🇦🇺 Haodi Wang": # Requested to use Chinese flag
-        name = "🇨🇳 Haodi Wang"
-    
-    if name == "🇮🇱 Rawad Aboud": # Gumroad api says he's from IL-TA (Tel Aviv, Israel), but he's Palestinian. See [this mail](message:<8C5D64EE-447A-4A65-89A4-27F99115C986@icloud.com>)
-        name = "🇵🇸 Rawad Aboud"
+    if   name == "🇩🇪 Aayam Khadka"                name = "🇳🇵 Aayam Khadka"      # [Aug 2025] Requested Nepal's flag in the message
+    elif name == "🇺🇦 Github Com/Alex Danilenko":  name = "🇺🇦 Alex Danilenko"    # [Aug 2025]
+    elif name == "🇦🇺 Haodi Wang":                 name = "🇨🇳 Haodi Wang"        # Requested to use Chinese flag
+    elif name == "🇮🇱 Rawad Aboud":                name = "🇵🇸 Rawad Aboud"       # Gumroad api says he's from IL-TA (Tel Aviv, Israel), but he's Palestinian. See [this mail](message:<8C5D64EE-447A-4A65-89A4-27F99115C986@icloud.com>)
 
     # Replace all spaces with non-breaking spaces
     name = name.replace(' ', nbsp)
