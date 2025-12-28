8671b1a30f07af28f6b4350a6026648c7501fe2c
Noah Nuebling <noah.n.public@gmail.com>
2025-12-28 23:48:07 +0100

    Update updateackurls.py

--- scripts/updateackurls/updateackurls.py
+++ scripts/updateackurls/updateackurls.py
@@ -2,7 +2,8 @@
 """ 
     Problem this solves: Whenever I add new language with importstrings.py, 
         I have to update every single `Acknowledgements.xcstrings > 2: translations` url for every language! 
-        This automates that. 
+        This automates part of that. 
+        Specifically, this edits the [markdown]({urls}) inside `Acknowledgements.xcstrings > 2: translations` so that they count up sequentially: {url_1}, {url_2}, {url_3}, ...
         [Dec 2025]
     Alternative solutions:
         - Change the way the `url_xx` replacement stuff works so I don't have to change them all when adding a new url at the start / middle [Dec 2025]
@@ -22,14 +23,11 @@ assert xcstrings_obj != None
 # Update urls inside "2: translations"
 key = "2: translations"
 for locale in xcstrings_obj["strings"][key]["localizations"]:
-    if locale == 'en': 
-        continue
+    if locale == 'en': continue
+    
     translation = xcstrings_obj["strings"][key]["localizations"][locale]["stringUnit"]["value"]
-    if "%#@" in translation: 
-        print(f"skipping pluralizable string: {translation}")
-        continue # Could handle plural variants, too, but we don't need that at the moment [Dec 2025]
-
     updated_translation = mfutils.replace_markdown_urls_with_format_specifiers(translation).md_string # Misuse replace_markdown_urls_with_format_specifiers() to make the url numbers in the translation sequential. ('Misuse' because the translations will already contain format specifiers) [Dec 2025]
+    
     xcstrings_obj["strings"][key]["localizations"][locale]["stringUnit"]["value"] = updated_translation
 
 # Write xcstrings file
